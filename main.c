58d0f0a000c4e3bf03dbe3898e0b63e048f9277c
harasimczukmateuszPS6 <harasimczukmateusz@gmail.com>
2026-01-06 16:49:36 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
 
 struct invention {
     char name[101];
-    char type[];
+    int type;
     int reliability;
+    int powerneeded;
+    char status[];
 };
 
 int main()
 {
-
+    printf("|||WELCOME TO WUNDERTECH DATA TERMINAL|||\n\n");
+    printf("Do you want to proceed? (Y/N): \n> ");
     return 0;
 }
