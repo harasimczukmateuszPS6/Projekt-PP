bff49da859f823aa2b6639b22510967b3fb4c9f0
harasimczukmateuszPS6 <harasimczukmateusz@gmail.com>
2026-01-06 17:59:43 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include <ctype.h>
 #include <math.h>
 
+#define MAX 100
+
 struct invention {
     char name[101];
     int type;
@@ -11,6 +13,38 @@ struct invention {
     int powerneeded;
     int status;
 };
+
+int nameCheck(char *name) {
+    for(int i = 0; name[i]; i++) {
+        if(!isalpha(name[i]))
+            return 0;
+    }
+    return 1;
+}
+
+void enterName(char *name) {
+    while(1) {
+        printf("\n> ");
+        scanf("%100s", name);
+        if(nameCheck(name))
+            return;
+        printf("Invalid entry! Try again:\n");
+    }
+}
+
+void newInvention(struct invention tab[], int *number) {
+    if(*number >= MAX) {
+        printf("No memory left!\n");
+        return;
+    }
+
+    printf("Enter name: \n");
+    enterName(tab[*number].name);
+
+    (*number)++;
+    printf("Invention added to database.\n");
+
+}
 
 int main()
 {
@@ -28,12 +62,21 @@ int main()
     printf("0 - Terminate program.\n");
     printf("\nChoose your option:\n");
 
+    struct invention tab[MAX];
+    int number = 0;
     int option;
     do {
         printf("> ");
         scanf("%d", &option);
         switch (option) {
             case 1: {
+                newInvention(tab, &number);
+                break;
+            }
+            case 2: {
+                break;
+            }
+            case 3: {
                 break;
             }
             case 0: {
